1ec42c2e2cbcc2d65b84cedb65e745013e075aed
Tianzi Cui <cuitianzi0@gmail.com>
2024-12-20 13:25:54 -0700

    improve RootLayoout

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -3,19 +3,18 @@ import Link from 'next/link'
 
 export default function Navigation() {
   return (
-    <ul>
-        <li>
-            <Link href="/">Home</Link>
+    <nav className='text-xl z-10'>
+    <ul className="flex gap-16 items-center">
+        <li >
+            <Link href="/cabins" className="hover:text-accent-400 transition-colors">Cabins</Link>
+        </li >
+        <li >
+            <Link href="/about" className="hover:text-accent-400 transition-colors">About</Link>
         </li>
         <li>
-            <Link href="/cabins">Cabins</Link>
-        </li>
-        <li>
-            <Link href="/about">About</Link>
-        </li>
-        <li>
-            <Link href="/account">Account</Link>
+            <Link href="/account" className="hover:text-accent-400 transition-colors">Account</Link>
         </li>
     </ul>
+    </nav>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
 
+import Header from "@/app/_components/Header";
 // config fonts
 import {Josefin_Sans} from "next/font/google";
 
@@ -29,15 +28,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${josefin.className}`}>
-        <header>
-        <Logo />
-        <Navigation />
-        </header>
-        <main>
+      <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+        <main className="max-w-7xl mx-auto ">
         {children}
-        </main>
-        <footer>Copyright by The Wild Oasis</footer>
+        </main></div>
+        
         </body>
     </html>
   )
