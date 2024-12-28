547528c183863dddd38d959f2845153bc918e5dc
Tianzi Cui <cuitianzi0@gmail.com>
2024-12-27 23:21:59 -0700

    built a GET api endpoint to return cabin and booked date

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,15 @@
+import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
+
+export async function GET(request, { params }) {
+  const { cabinId } = params;
+  try {
+    const [cabin, bookedDate] = await Promise.all([
+      getCabin(cabinId),
+      getBookedDatesByCabinId(cabinId),
+    ]);
+    return Response.json({ cabin, bookedDate });
+  } catch {
+    return Response.json({ message: "Cabin not found" }, { status: 500 });
+  }
+}
+// export async function POST() {}
