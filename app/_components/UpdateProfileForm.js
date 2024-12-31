06628e6bbd4d293bf5ecc829a5bb8ee66e2bb2ed
Tianzi Cui <cuitianzi0@gmail.com>
2024-12-31 12:42:42 -0700

    add a loading indicator for profileForm

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,3 +1,5 @@
+"use client";
+import { useFormStatus } from "react-dom";
 import { updateGuest } from "../_lib/actions";
 function UpdateProfileForm({ guest, children }) {
   // const countryFlag = "pt.jpg";
@@ -49,12 +51,22 @@ function UpdateProfileForm({ guest, children }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
   );
 }
 
 export default UpdateProfileForm;
+
+function Button() {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      disabled={pending}
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+    >
+      {pending ? "Updating..." : "Update profile"}
+    </button>
+  );
+}
