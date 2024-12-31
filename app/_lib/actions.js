1d2fd32231f21e23169bbbd6171ac1e8b66e7d20
Tianzi Cui <cuitianzi0@gmail.com>
2024-12-31 13:40:56 -0700

    delete a reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { auth, signIn, signOut } from "@/app/_lib/auth";
 import { supabase } from "./supabase";
 import { revalidatePath } from "next/cache";
+import { getBookings } from "./data-service";
 
 export async function signInAction() {
   // This function is meant to be used in the server component
@@ -40,4 +41,29 @@ export async function updateGuest(formData) {
   //revalidate the data
   //all data under the route will be cleared and re-fetched
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  //1. check if user is authenticated
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in.");
+  //2. check if user has the authority, users are onlt allowed to delete their own bookings
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+  if (!guestBookingIds.includes(bookingId)) {
+    throw new Error("You are not allowed to delete this booking.");
+  }
+  //if logged, delete reservation
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) {
+    console.error(error.message);
+    throw new Error("Booking could not be deleted");
+  }
+
+  //update UI after the server action has finished
+  revalidatePath("/account/reservation");
 }
